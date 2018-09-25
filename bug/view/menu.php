bb1544b2f24cc0e7d5c7d8c49127e6bef3b23e09
Matheus Oliveira <moguedes98@gmail.com>
2018-09-25 16:08:44 -0300

    adcionando o css do menu

--- bug/view/menu.php
+++ bug/view/menu.php
@@ -5,10 +5,10 @@
     <title>Adtile Fixed Nav</title>
     <meta name="author" content="Adtile">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="../css/styles.css">
     <!--[if lt IE 9]>
       <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-      <link rel="stylesheet" href="css/ie.css">
+      <link rel="stylesheet" href="../css/ie.css">
     <![endif]-->
     <script src="js/responsive-nav.js"></script>
   </head>
